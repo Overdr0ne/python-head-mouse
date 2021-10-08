09bdec5aac04a66e04c6336ad4bf2dd0a8cd8612
Overdr0ne <0v3rdr0n3@gmail.com>
2021-10-08 11:05:05 -0400

    add GPL 3 license

--- nose_tracker.py
+++ nose_tracker.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python
-# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
-#
-#   This example program shows how to find frontal human faces in a webcam stream using OpenCV.
-#   It is also meant to demonstrate that rgb images from Dlib can be used with opencv by just
-#   swapping the Red and Blue channels.
-#
-#   You can run this program and see the detections from your webcam by executing the
-#   following command:
-#       ./opencv_face_detection.py
-#
-#   This face detector is made using the now classic Histogram of Oriented
-#   Gradients (HOG) feature combined with a linear classifier, an image
-#   pyramid, and sliding window detection scheme.  This type of object detector
-#   is fairly general and capable of detecting many types of semi-rigid objects
-#   in addition to human faces.  Therefore, if you are interested in making
-#   your own object detectors then read the train_object_detector.py example
-#   program.  
-#
-#
-# COMPILING/INSTALLING THE DLIB PYTHON INTERFACE
-#   You can install dlib using the command:
-#       pip install dlib
-#
-#   Alternatively, if you want to compile dlib yourself then go into the dlib
-#   root folder and run:
-#       python setup.py install
-#
-#   Compiling dlib should work on any operating system so long as you have
-#   CMake installed.  On Ubuntu, this can be done easily by running the
-#   command:
-#       sudo apt-get install cmake
-#
-#   Also note that this example requires Numpy which can be installed
-#   via the command:
-#       pip install numpy
+# Copyright (C) 2021 overdr0ne
+
+# Author: overdr0ne
+# Version: 0.1
+# URL: https://github.com/Overdr0ne/python-head-mouse
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 import dlib
 import cv2
